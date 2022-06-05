2e860838c1322a8217d9de69af162224c2dc24d2
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-05 18:40:30 -0300

    Atualização

---
+++ modulo1/aula04/exercicio-se.php
@@ -0,0 +1,27 @@
+<?php
+
+$num =0;
+
+if ($num>0){
+    echo"positivo";
+} elseif ($num<0){
+    echo "Negativo";
+} else{
+    echo "Nulo";
+}
+
+echo "<br> Pronto, terminou!";
+
+/*
+MEU EXERCICIO
+$num = 0;
+if ($num > 0){
+    echo "positivo";
+} else{
+    if ($num < 0) {
+         echo "negativo";
+    } else{
+        echo "nulo";       
+    }
+}
+*/
