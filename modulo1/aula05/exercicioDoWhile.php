9f45e4c77bab0343665e9f4b4615e022c5ecfbd3
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-06 13:55:35 -0300

    atualizacao

---
+++ modulo1/aula05/exercicioDoWhile.php
@@ -0,0 +1,11 @@
+<?php
+//do while
+//Faça Enquanto
+$n=11;
+
+do{
+    echo "Ó o pente <br>";
+
+    $n++;
+
+    } while ($n<=10);
---
+++ modulo1/aula06/arrays.php
@@ -0,0 +1,24 @@
+<?php
+/*
+$aluno1 = "a";
+$aluno2 = "b";
+$aluno3 = "c";
+*/
+
+
+/*
+$alunos2 = array(
+'a',
+'b',
+'c'
+)
+*/
+
+$alunos = [
+    'Chiquim', //0
+    'Maria', //1
+    'Zezim' //2
+];
+
+echo $alunos[1];
+
