9f4e0e71845261ddaec1e6ffd61f4456cbad0ec5
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-06 23:44:15 -0300

    atualização

---
+++ modulo1/aula06/bandasForEach.php
@@ -0,0 +1,16 @@
+<?php
+
+$bandas = [
+    "Dream theater",
+    "Neo pi Neo",
+    "Exaltasamba",
+    'Iram Maiden'
+];
+
+echo '<ul>';
+
+    foreach ($bandas as $cadabanda) {
+        echo "<li>{$cadabanda}</li>";
+    }
+
+echo '<ul>';
