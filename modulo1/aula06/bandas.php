fb9b71a5294e28ea9218dd4a5f414eaf2c42834d
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-06 23:36:17 -0300

    atualizacao

---
+++ modulo1/aula06/bandas.php
@@ -0,0 +1,16 @@
+<?php
+
+$bandas = [
+    "Dream theater",
+    "Neo pi Neo",
+    "Exaltasamba",
+];
+
+echo '<ul>';
+
+    for ($n =0; $n <= 2; $n++){
+
+        echo '<li>' . $bandas[$n] . '</li>';
+    }
+
+echo '<ul>';
