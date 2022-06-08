4c56c843903abd00c6b088f8daaadec6614578ca
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-07 22:51:13 -0300

    atualização

--- modulo1/aula07/dados.php
+++ modulo1/aula07/dados.php
@@ -1,12 +1,12 @@
 <?php
 
 $dados =[
-    'chiquim',
-    'Silva',
-    'Fortaleza',
-    34,
-    2001,
-    'Azul',
+    'nome' =>'chiquim',
+    'sobrenome' =>'Silva',
+    'Cidade'=>'Fortaleza',
+    'idade' => 34,
+    'ano_formacao' => 2001,
+    'cor' => 'Azul',
 ];
 
 var_dump($dados);
