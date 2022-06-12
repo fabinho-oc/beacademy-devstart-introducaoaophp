e37990ecb711682af5ac6852ee78184d18305ae2
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-12 16:54:08 -0300

    Atualização

--- modulo1/aula09/index.php
+++ modulo1/aula09/index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Aula 09-Minha Navegação</title>
+        </head>
+
+        <body>
+
+            <!-- CSS only -->
+            <a><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></a>
+
+
+        </body>
+
+</html>
+
+
 <?php
 
 //captura de informações que consta na variavel global $_SERVER
@@ -5,6 +22,9 @@
 //echo $_SERVER['REQUEST_URI'];
 
 //Guarda a informação dentro de uma variavel
+
+
+
 
 $url = $_SERVER['REQUEST_URI'];
 
@@ -22,9 +42,11 @@ if ($url === '/') {
 } elseif ($url ==='/cadastro'){
 
     include 'telas/cadastro.php';
-    
+
 } else{
 
-    echo '<h1>Pagina não encontrada</h1>';
+    include 'telas/404.php';
 
 }
+
+?>
---
+++ modulo1/aula09/telas/404.php
@@ -0,0 +1,7 @@
+<h1>Pagina não encontrada</h1>
+
+<div class="container">
+        <div class="logo">
+          <img src="./telas/img/erro404.jpg" alt="Erro 404 - Pagina náo encontrada" width = 500 height = 500 />
+        </div>
+</div>
