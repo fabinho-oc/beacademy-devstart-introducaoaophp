66719994601d5b0f662b15b8919467f347fa5613
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-13 22:39:22 -0300

    atualização

--- modulo1/aula09/index.php
+++ modulo1/aula09/index.php
@@ -11,28 +11,35 @@ $url = $_SERVER['REQUEST_URI'];
 
 include 'telas/menu.php';
 include 'telas/head.php'; // cabeçalho da pagina
-
+include 'acoes.php'; // inclusão para funcionamento das funções criadas
 
 
 if ($url === '/') {
 
-    include 'telas/home.php';
+    // include 'telas/home.php'; => substituido pela função
+
+    home(); //é adotado a utilização da função no lugar da opção do include
 
 }elseif ($url ==='/login'){
 
-    include 'telas/login.php';
+    //include 'telas/login.php'; => substituido pela função
+
+    login();
 
 } elseif ($url ==='/cadastro'){
 
-    include 'telas/cadastro.php';
+    //include 'telas/cadastro.php'; => substituido pela função
+    cadastro();
 
 } elseif ($url ==='/listar'){
 
-    include 'telas/listar.php';
+    //include 'telas/listar.php'; => substituido pela função
+    listar();
 
 } else{
 
-    include 'telas/404.php';
+    //include 'telas/404.php'; => substituido pela função
+    erro404();
 
 }
 
