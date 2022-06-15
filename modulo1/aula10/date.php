3218a0eb6d8a4f26fc2ed70912e8ea7f580b06a6
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-14 23:04:17 -0300

    atualização

---
+++ modulo1/aula10/date.php
@@ -0,0 +1,10 @@
+<?php
+
+//echo date(); // traz a data atual do servidor
+
+$dia = date('d');
+
+$hora = date('H:i:s'); // MOSTRA A HORA QUE CONSTA NO SERVIDOR
+
+echo $dia . PHP_EOL;
+echo $hora . PHP_EOL;
--- modulo1/aula10/strings.php
+++ modulo1/aula10/strings.php
@@ -4,3 +4,9 @@ $nome = 'alessandro'; // Alessandro
 $sobrenome = 'FEITOSA'; // Feitoza
 
 $nome2 = 'chiquim da silva'; // Chiquim Da Silva
+
+echo "Nome: " . ucfirst($nome) . PHP_EOL; // ucfirst=> upperCaseFirst (caixa alta ou maiusculo - apenas a primeira letra para Maiusculo)
+
+echo "Sobrenome: " . ucfirst(strtolower($sobrenome)) . PHP_EOL; // strtolower=> string to LOWER CASE, string para minusculo
+
+echo "Nome2: " . ucwords($nome2) . PHP_EOL; // Pega a letra de cada palavra e coloca em maiusculo
