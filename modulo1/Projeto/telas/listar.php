5dea752385f44e569aa4e827eaadd18e3bde21d0
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-15 12:55:11 -0300

    Atualização

--- modulo1/Projeto/telas/listar.php
+++ modulo1/Projeto/telas/listar.php
@@ -1,6 +1,18 @@
 <h1>Listar Contatos</h1>
 
 
-<table>
+<table Class = "table table-hover table -striped">
 
-</table>
+    <thead Class = "table-dark">
+
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+        </tr>
+
+    </thead>
+
+
+
+</table> 
